62bc3ca723f864e84416fa0d254c81e4ced6f2b4
Manuel Blanco <manuel.blanco77@gmail.com>
2020-11-02 15:23:14 -0300

    Redux setup, proxy setup, reducers index created and postReducer implemented

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import {postReducer} from './posts';
 
 export default combineReducers({
-    postReducer
+    posts: postReducer
 });
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,10 +2,13 @@
 export const postReducer = (posts = [] , action) => {
 
     switch (action.type) {
+
         case 'FETCH_ALL':
-            return posts;
+            return action.payload;
+
         case 'CREATE':
             return posts;
+
         default:
             return posts;
     }
