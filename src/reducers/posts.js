9568b7890001ed90312e333181648b2d3b9e83f6
Manuel Blanco <manuel.blanco77@gmail.com>
2020-11-02 14:58:20 -0300

    Initial Redux setup

---
+++ src/reducers/posts.js
@@ -0,0 +1,12 @@
+
+export const postReducer = (posts = [] , action) => {
+
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return posts;
+        case 'CREATE':
+            return posts;
+        default:
+            return posts;
+    }
+}
