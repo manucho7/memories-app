b51e7821dd3e027516aac47de1a1170fd9155dbc
Manuel Blanco <manuel.blanco77@gmail.com>
2020-11-07 09:27:41 -0300

    Update Functionality, post update in reducer, action, form button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = () => {
-    const classes = useStyles();
-    
-    const dispatch = useDispatch();
+//GET the current ID of post
+
+const Form = ({setCurrentId, currentId}) => {
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
@@ -18,9 +18,24 @@ const Form = () => {
         selectedFile: ''
     });
 
+    const post = useSelector(( state ) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+    const classes = useStyles();
+    
+    const dispatch = useDispatch();
+    
+    useEffect(() =>{
+        if(post) setPostData(post);
+    },[post])
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch( createPost( postData ) );
+
+        if(currentId) {
+            dispatch( updatePost( currentId ,postData ) );
+        } else {
+            dispatch( createPost( postData ) );
+        }
+
     }
 
     const clear = () => {
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,12 +2,15 @@
 export const postReducer = (posts = [] , action) => {
 
     switch (action.type) {
+        case 'UPDATE':
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
 
         case 'FETCH_ALL':
             return action.payload;
 
         case 'CREATE':
             return [...postReducer, action.payload];
+
 
         default:
             return posts;
