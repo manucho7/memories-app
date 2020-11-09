9ccb1308d415d22bc00535f892824200771734e0
Manuel Blanco <manuel.blanco77@gmail.com>
2020-11-09 09:43:45 -0300

    Action constants directory and implementation in actions && reducers

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,25 +1,24 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
 
 //Action Creators
 //Functions that return actions
 
 export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: FETCH_ALL, payload: data });
 
-        try {
-            const { data } = await api.fetchPosts();
-            dispatch({ type: 'FETCH_ALL', payload: data });
-
-        } catch (error) {
-            console.log(error.message);
-        }
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 export const createPost = (post) => async (dispatch) => {
-
     try {
         
         const { data } = await api.createPost(post);
-        dispatch({ type: 'CREATE', payload: data });
+        dispatch({ type: CREATE, payload: data });
 
     } catch (error) {
         console.log( error.message );
@@ -27,44 +26,37 @@ export const createPost = (post) => async (dispatch) => {
 }
 
 export const updatePost = (id, post) => async(dispatch) => {
-
     try {
         
         const { data } = await api.updatePost(id, post);
 
-        dispatch({ type: 'UPDATE', payload: data});
+        dispatch({ type: UPDATE, payload: data});
 
     } catch (error) {
         console.log(error);
     }
-
 }
 
 export const deletePost = (id) => async(dispatch) => {
-
     try {
         
         await api.deletePost(id); 
 
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
 
     } catch (error) {
         console.log(error);
     }
-
 }
 
 export const likePost = (id) => async(dispatch) => {
-
     try {
         
         const { data } = await api.likePost(id);
 
-        dispatch({ type: 'LIKE', payload: data});
+        dispatch({ type: UPDATE, payload: data});
 
     } catch (error) {
         console.log(error);
     }
-
-
 }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,20 +1,20 @@
+import { CREATE, DELETE, FETCH_ALL, UPDATE } from "../constants/actionTypes";
 
 export const postReducer = (posts = [] , action) => {
 
     switch (action.type) {
 
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
 
-        case 'CREATE':
+        case CREATE:
             return [...postReducer, action.payload];
 
-        case 'DELETE':
+        case DELETE:
             //keep all posts except that post whose id matches the action.payload
             return posts.filter((post) => post._id !== action.payload );
 
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
             return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
 
         default:
