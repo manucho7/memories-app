9dca768ed236aa07171b13776f0882f81297d48a
Manuel Blanco <manuel.blanco77@gmail.com>
2020-11-09 09:28:30 -0300

    Mobile responsiveness for small devices so that <Form/> component appears first

--- src/App.js
+++ src/App.js
@@ -27,7 +27,13 @@ const App = () => {
 
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}> 
+                    <Grid 
+                        container 
+                        justify="space-between" 
+                        alignItems="stretch" 
+                        spacing={3}
+                        className={classes.mainContainer}
+                    > 
                         <Grid item xs={12} sm={7}>
                             <Posts setCurrentId={ setCurrentId }/>
                         </Grid>
